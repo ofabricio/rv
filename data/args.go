690f8a1dd056638d41bac48e3644e30c35706db0
ofabricio <fabriciogiac@gmail.com>
2026-01-10 08:27:02 -0300

    change: melhora descrição de argumentos

--- data/args.go
+++ data/args.go
@@ -11,7 +11,7 @@ func (c *Carteira) CommandLine() {
 
 	flag.Usage = func() {
 		fmt.Println()
-		fmt.Println("  Usage: rv")
+		fmt.Println("  Usage: rv [flags]")
 		fmt.Println()
 		flag.PrintDefaults()
 	}
@@ -19,7 +19,7 @@ func (c *Carteira) CommandLine() {
 	file := flag.String("file", "db.ndjson", "arquivo de operações")
 	frmt := flag.String("format", "table", "mostra resultado no formato especificado (table, csv)")
 	flag.StringVar(&c.Param.FormatoData, "date-format", "2006-01-02", "formato de data (ex. 02/01/2006)")
-	flag.BoolVar(&c.Param.MostrarValorExato, "exact-value", false, "mostra valores exatos (ex. 0,14952765 em vez de 0,15)")
+	flag.BoolVar(&c.Param.MostrarValorExato, "exact-value", false, "mostra valores exatos (ex. 0,14952765 em vez de 0,15) (default false)")
 	flag.IntVar(&c.Param.FiltrarAno, "filter-year", 0, "filtra operações por ano (YYYY para mostrar o ano em questão; 0 para mostrar todos os anos; -1 para mostrar apenas o ano atual) (default 0)")
 
 	nota := flag.Bool("notas", false, "importa notas de corretagem de arquivos pdf que estiverem no diretório corrente")
